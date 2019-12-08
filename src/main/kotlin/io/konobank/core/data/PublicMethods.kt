72977d7bfeb53cade40bb648462e36a4d760f16a
Roman Korostenskyi <romankorostensky@gmail.com>
2019-12-09 00:22:46 +0200

    feat: add coroutines to PublicMethods.kt

--- src/main/kotlin/io/konobank/core/data/PublicMethods.kt
+++ src/main/kotlin/io/konobank/core/data/PublicMethods.kt
@@ -2,6 +2,7 @@ package io.konobank.core.data
 
 import io.konobank.core.Client
 import io.konobank.core.model.CurrencyInfo
+import kotlinx.coroutines.Dispatchers
 import kotlinx.serialization.ImplicitReflectionSerializer
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonConfiguration
@@ -9,5 +10,5 @@ import kotlinx.serialization.parseList
 import java.net.URL
 
 @ImplicitReflectionSerializer
-fun Client.fetchBankCurrency(): List<CurrencyInfo> =
-    Json(JsonConfiguration.Stable).parseList(URL("$BASE_URL$CURRENCY_INFO").readText())
+suspend fun Client.fetchBankCurrency(): List<CurrencyInfo> =
+    with(Dispatchers.IO) { Json(JsonConfiguration.Stable).parseList(URL("$BASE_URL$CURRENCY_INFO").readText()) }
