b371d3aa38931fcd9c9778caa605fb3a8bb7c274
Roman Korostenskyi <romankorostensky@gmail.com>
2020-01-27 09:35:37 +0200

    refactor: Kotlin Coroutines in requests
    Use Ktor client to make HTTP requests

--- src/main/kotlin/io/konobank/core/data/PublicMethods.kt
+++ src/main/kotlin/io/konobank/core/data/PublicMethods.kt
@@ -2,13 +2,14 @@ package io.konobank.core.data
 
 import io.konobank.core.Client
 import io.konobank.core.model.CurrencyInfo
-import kotlinx.coroutines.Dispatchers
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
 import kotlinx.serialization.ImplicitReflectionSerializer
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonConfiguration
 import kotlinx.serialization.parseList
-import java.net.URL
 
 @ImplicitReflectionSerializer
-suspend fun Client.fetchBankCurrency(): List<CurrencyInfo> =
-    with(Dispatchers.IO) { Json(JsonConfiguration.Stable).parseList(URL("$BASE_URL$CURRENCY_INFO").readText()) }
+suspend fun Client.fetchBankCurrency(): List<CurrencyInfo> {
+    return Json(JsonConfiguration.Stable).parseList(HttpClient().get("$BASE_URL$CURRENCY_INFO"))
+}
