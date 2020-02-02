ac1f9224546118939e470ccc769e4e130c3d9398
Roman Korostenskyi <romankorostensky@gmail.com>
2020-02-02 23:38:32 +0200

    refactor: `HttpClient` init

--- src/main/kotlin/io/konobank/core/Client.kt
+++ src/main/kotlin/io/konobank/core/Client.kt
@@ -1,6 +1,10 @@
 package io.konobank.core
 
+import io.ktor.client.HttpClient
+
 class Client() {
+
+    internal val client = HttpClient()
 
     var token: String = ""
 
--- src/main/kotlin/io/konobank/core/data/PublicMethods.kt
+++ src/main/kotlin/io/konobank/core/data/PublicMethods.kt
@@ -2,7 +2,6 @@ package io.konobank.core.data
 
 import io.konobank.core.Client
 import io.konobank.core.model.CurrencyInfo
-import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 import kotlinx.serialization.ImplicitReflectionSerializer
 import kotlinx.serialization.json.Json
@@ -11,5 +10,5 @@ import kotlinx.serialization.parseList
 
 @ImplicitReflectionSerializer
 suspend fun Client.fetchBankCurrency(): List<CurrencyInfo> {
-    return Json(JsonConfiguration.Stable).parseList(HttpClient().get("$BASE_URL$CURRENCY_INFO"))
+    return Json(JsonConfiguration.Stable).parseList(client.get("$BASE_URL$CURRENCY_INFO"))
 }
