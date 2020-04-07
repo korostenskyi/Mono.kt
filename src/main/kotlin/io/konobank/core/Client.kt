3148f4a14059903f65f716a7200af8cc8d44c4c5
Roman Korostenskyi <romankorostensky@gmail.com>
2020-04-07 22:37:30 +0300

    refactor: token is now set default to all requests
    update libs version

--- src/main/kotlin/io/konobank/core/Client.kt
+++ src/main/kotlin/io/konobank/core/Client.kt
@@ -1,12 +1,20 @@
 package io.konobank.core
 
 import io.ktor.client.HttpClient
+import io.ktor.client.features.defaultRequest
+import io.ktor.client.request.header
 
 class Client() {
 
-    internal val client = HttpClient()
-
     var token: String = ""
+
+    internal val client = HttpClient {
+        defaultRequest {
+            if (token.isNotBlank()) {
+                header("X-Token", token)
+            }
+        }
+    }
 
     constructor(token: String): this() {
         this.token = token
--- src/main/kotlin/io/konobank/core/data/PrivateMethods.kt
+++ src/main/kotlin/io/konobank/core/data/PrivateMethods.kt
@@ -8,44 +8,34 @@ import io.konobank.core.model.StatementItem
 import io.konobank.core.model.Status
 import io.konobank.core.model.UserInfo
 import io.ktor.client.request.get
-import io.ktor.client.request.header
 import io.ktor.client.request.post
 import kotlinx.serialization.ImplicitReflectionSerializer
-import kotlinx.serialization.UnstableDefault
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonConfiguration
 import kotlinx.serialization.parseList
 
-suspend fun Client.fetchUserInfo(token: String = this.token): UserInfo {
+suspend fun Client.fetchUserInfo(): UserInfo {
     return Json(JsonConfiguration.Stable).parse(
         UserInfo.serializer(),
-        client.get(CLIENT_INFO_URL) {
-            header("X-Token", token)
-        }
+        client.get(CLIENT_INFO_URL)
     )
 }
 
 // TODO: Investigate this method
-suspend fun Client.setWebHook(token: String = this.token): Status {
+suspend fun Client.setWebHook(): Status {
     return Json(JsonConfiguration.Stable).parse(
         Status.serializer(),
-        client.post(SET_WEBHOOK_URL) {
-            header("X-Token", token)
-        }
+        client.post(SET_WEBHOOK_URL)
     )
 }
 
-@UnstableDefault
 @ImplicitReflectionSerializer
 suspend fun Client.fetchStatementItems(
-    token: String = this.token,
     account: String,
     from: String,
     to: String = System.currentTimeMillis().toString()
 ): List<StatementItem> {
-    return Json(JsonConfiguration(strictMode = false)).parseList(
-        client.get("$STATEMENT_ITEMS_URL$account/$from/$to/") {
-            header("X-Token", token)
-        }
+    return Json(JsonConfiguration(ignoreUnknownKeys = true)).parseList(
+        client.get("$STATEMENT_ITEMS_URL$account/$from/$to/")
     )
 }
