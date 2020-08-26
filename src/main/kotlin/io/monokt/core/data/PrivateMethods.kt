a2a5a3520476ff4a8864a5be1dfe788a855ec3c5
Roman Korostenskyi <romankorostensky@gmail.com>
2020-08-26 15:47:28 +0300

    refactor: update libs and entities
    Kotlin update to 1.4.0
    Ktor update to 1.4.0
    Kotlinx Serialization update to 1.0.0-RC
    Kotlinx Coroutines update to 1.3.9

--- src/main/kotlin/io/monokt/core/data/PrivateMethods.kt
+++ src/main/kotlin/io/monokt/core/data/PrivateMethods.kt
@@ -1,5 +1,7 @@
 package io.monokt.core.data
 
+import io.ktor.client.request.get
+import io.ktor.client.request.post
 import io.monokt.core.Client
 import io.monokt.core.data.destinations.CLIENT_INFO_URL
 import io.monokt.core.data.destinations.SET_WEBHOOK_URL
@@ -7,35 +9,25 @@ import io.monokt.core.data.destinations.STATEMENT_ITEMS_URL
 import io.monokt.core.model.StatementItem
 import io.monokt.core.model.Status
 import io.monokt.core.model.UserInfo
-import io.ktor.client.request.get
-import io.ktor.client.request.post
-import kotlinx.serialization.ImplicitReflectionSerializer
+import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonConfiguration
-import kotlinx.serialization.parseList
 
 suspend fun Client.fetchUserInfo(): UserInfo {
-    return Json(JsonConfiguration.Stable).parse(
-        UserInfo.serializer(),
-        client.get(CLIENT_INFO_URL)
-    )
+    val infoString = client.get<String>(CLIENT_INFO_URL)
+    return Json.decodeFromString(infoString)
 }
 
 // TODO: Investigate this method
 suspend fun Client.setWebHook(): Status {
-    return Json(JsonConfiguration.Stable).parse(
-        Status.serializer(),
-        client.post(SET_WEBHOOK_URL)
-    )
+    val statusString = client.post<String>(SET_WEBHOOK_URL)
+    return Json.decodeFromString(statusString)
 }
 
-@ImplicitReflectionSerializer
 suspend fun Client.fetchStatementItems(
     account: String,
     from: String,
     to: String = System.currentTimeMillis().toString()
 ): List<StatementItem> {
-    return Json(JsonConfiguration(ignoreUnknownKeys = true)).parseList(
-        client.get("$STATEMENT_ITEMS_URL$account/$from/$to/")
-    )
+    val itemsString = client.get<String>("$STATEMENT_ITEMS_URL$account/$from/$to/")
+    return Json.decodeFromString(itemsString)
 }
